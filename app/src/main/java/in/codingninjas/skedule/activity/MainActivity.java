44c5f1bc6b9562b397afaf7369d14d36bb34de6e
LALIT GULIA <lalitgulia397@gmail.com>
2016-07-31 13:59:23 +0530

    Initial commit

---
+++ app/src/main/java/in/codingninjas/skedule/activity/MainActivity.java
@@ -0,0 +1,51 @@
+package in.codingninjas.skedule.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.widget.FrameLayout;
+
+
+import com.yalantis.guillotine.animation.GuillotineAnimation;
+
+import in.codingninjas.skedule.R;
+
+public class MainActivity extends AppCompatActivity {
+    private static final long RIPPLE_DURATION = 250;
+    FrameLayout root;
+    Toolbar toolbar;
+    View contentHamburger;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        root = (FrameLayout) findViewById(R.id.root);
+        contentHamburger = findViewById(R.id.content_hamburger);
+
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setTitle(null);
+        }
+
+        View guillotineMenu = LayoutInflater.from(this).inflate(R.layout.guillotine, null);
+        root.addView(guillotineMenu);
+
+        new GuillotineAnimation.GuillotineBuilder(guillotineMenu, guillotineMenu.findViewById(R.id.guillotine_hamburger), contentHamburger)
+                .setStartDelay(RIPPLE_DURATION)
+                .setActionBarViewForAnimation(toolbar)
+                .setClosedOnStart(true)
+                .build();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+}
+
